6ec4528abe060596b92003cbf9a8526ba47ae1ab
Greg Holmes <git@greg.holmes.name>
2023-07-28 21:34:12 +0100

    Move server into its own package

--- cmd/main.go
+++ cmd/main.go
@@ -1,19 +1,13 @@
 package main
 
 import (
-	"context"
-	"errors"
 	"flag"
-	"fmt"
 	"github.com/csmith/envflag"
-	"github.com/go-chi/chi/v5"
 	"github.com/greboid/dsp/internal"
 	"golang.org/x/exp/slog"
-	"net/http"
 	"os"
 	"os/signal"
 	"syscall"
-	"time"
 )
 
 var (
@@ -25,38 +19,24 @@ var (
 func main() {
 	envflag.Parse()
 	var p *internal.Proxy
+	var s *internal.Server
 	var err error
+
+	shutdownSignal := make(chan os.Signal, 1)
+	signal.Notify(shutdownSignal, syscall.SIGINT, syscall.SIGTERM)
+
 	if p, err = internal.NewProxy(*permissibleKillSignals, *realSock); err != nil {
 		slog.Error("socket does not exist", "socket", *realSock)
 		return
 	}
-	router := chi.NewRouter()
-	router.Post("/containers/{id}/kill", p.ContainerKill)
-	router.Post("/{apiversion}/containers/{id}/kill", p.ContainerKill)
-	router.Post("/*", p.AccessDenied)
-	router.Get("/*", p.PassToSocket)
-	server := &http.Server{
-		Addr:    fmt.Sprintf("0.0.0.0:%d", *proxyPort),
-		Handler: router,
-	}
-	sigChan := make(chan os.Signal, 1)
-	signal.Notify(sigChan, syscall.SIGINT, syscall.SIGTERM)
-	shutdownCtx, shutdownRelease := context.WithTimeout(context.Background(), 2*time.Second)
-	defer shutdownRelease()
 
-	go runServer(server, sigChan)
-
-	<-sigChan
-	if err = server.Shutdown(shutdownCtx); err != nil {
-		slog.Error("Shutdown error", "error", err)
+	if s, err = internal.NewServer(p, *proxyPort, shutdownSignal); err != nil {
+		slog.Error("unable to create server", "error", err)
+		return
 	}
+
+	s.Start()
+	<-shutdownSignal
+	s.Shutdown()
 	slog.Info("Exiting")
-}
-
-func runServer(server *http.Server, c chan os.Signal) {
-	slog.Info("Starting server", "url", fmt.Sprintf("http://0.0.0.0:%d", *proxyPort))
-	if err := server.ListenAndServe(); !errors.Is(err, http.ErrServerClosed) {
-		slog.Error("Server error", "error", err)
-		c <- syscall.SIGINT
-	}
 }
---
+++ internal/http.go
@@ -0,0 +1,61 @@
+package internal
+
+import (
+	"context"
+	"errors"
+	"fmt"
+	"github.com/go-chi/chi/v5"
+	"golang.org/x/exp/slog"
+	"net/http"
+	"os"
+	"syscall"
+	"time"
+)
+
+type Server struct {
+	p          *Proxy
+	port       int
+	httpServer *http.Server
+	signal     chan os.Signal
+}
+
+func NewServer(p *Proxy, port int, signal chan os.Signal) (*Server, error) {
+	s := &Server{
+		p:      p,
+		port:   port,
+		signal: signal,
+	}
+	r := s.createRouter(p)
+	s.httpServer = &http.Server{
+		Addr:    fmt.Sprintf("0.0.0.0:%d", port),
+		Handler: r,
+	}
+	return s, nil
+}
+
+func (s *Server) createRouter(p *Proxy) *chi.Mux {
+	router := chi.NewRouter()
+	router.Post("/containers/{id}/kill", p.ContainerKill)
+	router.Post("/{apiversion}/containers/{id}/kill", p.ContainerKill)
+	router.Post("/*", p.AccessDenied)
+	router.Get("/*", p.PassToSocket)
+	return router
+}
+
+func (s *Server) Shutdown() {
+	shutdownCtx, shutdownRelease := context.WithTimeout(context.Background(), 2*time.Second)
+	defer shutdownRelease()
+	if err := s.httpServer.Shutdown(shutdownCtx); err != nil {
+		slog.Error("Shutdown error", "error", err)
+	}
+}
+
+func (s *Server) Start() {
+	go func() {
+		slog.Info("Starting server", "url", fmt.Sprintf("http://0.0.0.0:%d", s.port))
+		if err := s.httpServer.ListenAndServe(); !errors.Is(err, http.ErrServerClosed) {
+			slog.Error("Server error", "error", err)
+			s.signal <- syscall.SIGINT
+		}
+	}()
+}
